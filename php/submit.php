d13cad0c8215265aeec47d72667daf0b920a4115
pranav6969 <65535901+pranav6969@users.noreply.github.com>
2020-07-05 03:03:27 +0530

    Update submit.php

--- php/submit.php
+++ php/submit.php
@@ -9,34 +9,7 @@
         $to = "karanmishra9478@gmail.com"; // You can change here your Email
         $subject = "'$name' has been sent a mail from Absolute-Automations Website"; // This is your subject
          
-        // HTML Message Starts here
-        $message ="
-        <html>
-            <body>
-                <table style='width:600px;'>
-                    <tbody>
-                        <tr>
-                            <td style='width:150px'><strong>Name: </strong></td>
-                            <td style='width:400px'>$name</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Email ID: </strong></td>
-                            <td style='width:400px'>$email_id</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Mobile No: </strong></td>
-                            <td style='width:400px'>$mobile_no</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Message: </strong></td>
-                            <td style='width:400px'>$msg</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </body>
-        </html>
-        ";
-        // HTML Message Ends here
+
          
         // Always set content-type when sending HTML email
         $headers = "MIME-Version: 1.0" . "\r\n";
