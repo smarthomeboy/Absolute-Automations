d79f612411c65a475ba7fa4b62ed3386e6757709
pranav6969 <65535901+pranav6969@users.noreply.github.com>
2020-07-05 03:17:45 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,8 +1,8 @@
 <?php
     if(isset($_POST['submit-form']))
     {
-        $name = $_POST['username']; // Get Name value from HTML Form
-        $email_id = $_POST['email']; // Get Email Value
+        $name = $_REQUEST['username']; // Get Name value from HTML Form
+        $email_id = $_REQUEST['email']; // Get Email Value
         $msg = $_POST['contact_message']; // Get Message Value
          
         $to = "karanmishra9478@gmail.com"; // You can change here your Email
@@ -12,7 +12,7 @@
          
         // Always set content-type when sending HTML email
         $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= "Content-type:text/javascript;charset=UTF-8" . "\r\n";
  
         // More headers
         $headers .= 'From: Admin <karanmishra9478@gmail.com>' . "\r\n"; // Give an email id on which you want get a reply. User will get a mail from this email id
