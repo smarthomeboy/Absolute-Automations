4ea6a96721cba8b997158734ae9aef5a1675419a
pranav6969 <65535901+pranav6969@users.noreply.github.com>
2020-07-05 03:13:17 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,42 +1,14 @@
 <?php
-    if(isset($_POST['submit']))
+    if(isset($_POST['submit-form']))
     {
-        $name = $_POST['name']; // Get Name value from HTML Form
+        $name = $_POST['username']; // Get Name value from HTML Form
         $email_id = $_POST['email']; // Get Email Value
-        $mobile_no = $_POST['mobile']; // Get Mobile No
-        $msg = $_POST['message']; // Get Message Value
+        $msg = $_POST['contact_message']; // Get Message Value
          
         $to = "karanmishra9478@gmail.com"; // You can change here your Email
-        $subject = "'$name' has been sent a mail from Absolute-Automations Website"; // This is your subject
+        $subject = "'$name' has sent a mail from Absolute-Automations Website"; // This is your subject
          
-        // HTML Message Starts here
-        $message ="
-        <html>
-            <body>
-                <table style='width:600px;'>
-                    <tbody>
-                        <tr>
-                            <td style='width:150px'><strong>Name: </strong></td>
-                            <td style='width:400px'>$name</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Email ID: </strong></td>
-                            <td style='width:400px'>$email_id</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Mobile No: </strong></td>
-                            <td style='width:400px'>$mobile_no</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Message: </strong></td>
-                            <td style='width:400px'>$msg</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </body>
-        </html>
-        ";
-        // HTML Message Ends here
+
          
         // Always set content-type when sending HTML email
         $headers = "MIME-Version: 1.0" . "\r\n";
